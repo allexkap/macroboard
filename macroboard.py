676ef4686a7139b8f08ba600f84f1b758ce3ee89
allexkap <aekpstn@gmail.com>
2023-03-08 15:37:43 +0300

    add special keys support

--- macroboard.py
+++ macroboard.py
@@ -1,41 +1,87 @@
-result = bytearray()
+from serial import Serial
+from keys import *
 
-def join(line, sep):
+
+opcodes = {
+    'NOTHING':     0x00,
+    'RESERVED':   0xF0,
+    'PRESS':      0xF1,
+    'RELEASE':    0xF2,
+    'RELEASEALL': 0xF3,
+    'DELAY':      0xF4,
+}
+states = {
+    'IDLE': 0xF8,
+    'NAME': 0xF9,
+    'DATA': 0xFA,
+    'DONE': 0xFB,
+    'OPEN': 0xFC,
+}
+
+
+def _load(data, tty='/dev/ttyACM0'):
+    with Serial(tty, 115200, timeout=0.1) as box:
+        box.write(data)
+        echo = bytes()
+        while not echo:
+            echo = box.readall()
+        print(echo.decode())
+
+def _createFile(filename):
+    data = bytearray()
+    data.append(states['NAME'])
+    data.extend(filename.encode(encoding='ascii'))
+    data.append(states['DATA'])
+    data.extend(actions)
+    data.append(states['DONE'])
+    return data
+
+def _join(line, sep):
     for ch in line:
-        if sep:
-            result.append(sep)
-        result.append(ord(ch))
+        if sep: actions.append(sep)
+        actions.append(ord(ch))
+
+
+def clear():
+    actions.clear()
 
 def write(line):
-    join(line, 0x00)
+    _join(line, opcodes['NOTHING'])
 
-def press(line, dt=None):
-    join(line, 0x81)
-    
+def press(line):
+    _join(line, opcodes['PRESS'])
+
 def release(line):
-    join(line, 0x82)
+    _join(line, opcodes['RELEASE'])
 
 def releaseAll():
-    result.append(0x83)
+    actions.append(opcodes['RELEASEALL'])
 
-def delay(dt):
-    while dt > 0:
-        result.append(0x84)
-        result.append(min(dt, 255))
-        dt -= 255
+def delay(time):
+    while time > 0:
+        if opcodes['DELAY'] == time:
+            time -= 1
+        actions.append(opcodes['DELAY'])
+        actions.append(min(time, 255))   # fix
+        time -= 255
 
-def load(tty=None):
-    pass
+def save(filename='default', *args, **kwargs):
+    data = _createFile(filename)
+    _load(data, *args, **kwargs)
+
+def remove(filename=None, *args, **kwargs):
+    if filename:
+        data = _createFile(filename)
+        data.pop()
+    else:
+        data = bytes((states['OPEN'],))
+    _load(data, *args, **kwargs)
 
 
 
-import serial
-from time import sleep
+actions = bytearray()
 
-
-data = bytes(ord(i) for i in '\x91serial\x92wow!\n\x94')
-
-with serial.Serial('/dev/ttyACM0', 115200) as box:
-    box.write(data)
-    while True:
-        print(*(chr(ch) for ch in box.read()), end='')
+if __name__ == '__main__':
+    write('macroboard')
+    releaseAll()
+    save()
